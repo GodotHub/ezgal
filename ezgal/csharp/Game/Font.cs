0066dde36c584b04f78bc0f955795f4b15663357
cryingn <cryingnights7v@qq.com>
2025-12-30 19:18:59 +0800

    fix image & tween, jump needs fixed

--- ezgal/csharp/Game/Font.cs
+++ ezgal/csharp/Game/Font.cs
@@ -4,57 +4,62 @@ using System;
 public partial class Font : Control
 {
 	Tween tween;
-	RichTextLabel text;
-	Dictionary dic_node;
+	[Export]
+	RichTextLabel TextNode { get; set; }
+	[Export]
+	Dictionary DictionaryScene { get; set; }
 
 	[Signal]
 	public delegate void StartGameEventHandler();
 
 	public override void _Ready()
 	{
-		dic_node = GetNode<Dictionary>("dictionary");
-		text = GetNode<RichTextLabel>("text");
 		tween = GetTree().CreateTween();
-		_hide();
+		Hide();
 	}
 	
-	// 隐藏对话框
-	public void _hide()
+	/* 隐藏对话框
+	public new void Hide()
 	{
-		Hide();
+		base.Hide();
 	}
 
 	// 显示对话框
-	public void _show()
+	public new void Show()
 	{
-		Show();
+		base.Show();
 	}
+	*/
 
 	// 设置对话框文本
 	public void SetText(string text_data, bool add_data)
 	{
 		if ( tween.IsRunning() )
 		{
-			GD.Print("kill");
 			tween.Kill();
-			text.VisibleCharacters = text.Text.Length;
+			TextNode.VisibleCharacters = TextNode.Text.Length;
 
 		}
-		if (add_data && text.Text.Split("\n").Length < 12)
+		if (add_data && TextNode.Text.Split("\n").Length < 12)
 		{
-			if (text.Text.EndsWith(" »"))
+			if (TextNode.Text.EndsWith(" »"))
 			{
-				text.Text = text.Text.Replace(" »", "\n");
+				TextNode.Text = TextNode.Text.Replace(" »", "\n");
 			}
-			text.Text += $"{text_data} »";
+			TextNode.Text += $"{text_data} »";
 		}
 		else
 		{
-			text.VisibleCharacters = 0;
-			text.Text = $"{text_data} »";
+			TextNode.VisibleCharacters = 0;
+			TextNode.Text = $"{text_data} »";
 		};
 		tween = GetTree().CreateTween();
-		tween.TweenProperty(text, "visible_characters", text.Text.Length, text_data.Length * Global.text_speed);
+		tween.TweenProperty(
+				TextNode,
+				"visible_characters", 
+				Tools.RemoveBBCode(TextNode.Text).Length,
+				Tools.RemoveBBCode(text_data).Length * Global.text_speed
+				);
 	}
 
 	// 鼠标点击事件
@@ -83,7 +88,7 @@ public partial class Font : Control
 	// 跳转到专业词汇文本事件
 	public void _on_text_meta_clicked(Variant meta)
 	{
-		Global.LoadDictionary(dic_node, meta);
+		Global.LoadDictionary(DictionaryScene, meta);
 	}
 
 }
